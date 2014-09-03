8889a29c5eae87eed5c186278e0b02a3ff2e17c1
Cameron Klein <cameronklein@github.com>
2014-09-02 19:46:56 -0700

    Initial Commit

---
+++ Pool Calc/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  Pool Calc
+//
+//  Created by Cameron Klein on 9/2/14.
+//  Copyright (c) 2014 Cameron Klein. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+                            
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
